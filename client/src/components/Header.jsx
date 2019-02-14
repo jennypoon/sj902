4b12b187808a5af2953a80c37b42c0bf9f4d1d0c
Jenny Poon <jenny.poon@live.ca>
2019-02-13 18:57:06 -0800

    added image

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Fall from '../images/fall.jpg'
+import Stanley from '../images/cityline2.jpg'
+function Header() {
+    return (
+      <div className="landing">
+      </div>
+    )
+}
+
+export default Header;
