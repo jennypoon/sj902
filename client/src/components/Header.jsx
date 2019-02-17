ee62200630d874ec8b0997225f88c8eefee75057
Jenny Poon <jenny.poon@live.ca>
2019-02-16 21:57:31 -0800

    removed unused components

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import Fall from '../images/fall.jpg'
-import Stanley from '../images/cityline2.jpg'
+
 function Header() {
     return (
       <div className="landing">
